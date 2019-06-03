52f385aa1562b5fe0fa1730223ea7b4d57719da0
Soha <Soha.Mourad@triosstudent.com>
2019-06-03 19:24:12 -0400

    first commit

---
+++ imageFilters/FiltersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FiltersViewController.swift
+//  imageFilters
+//
+//  Created by Soha Mourad on 2019-05-30.
+//  Copyright © 2019 Soha Mourad. All rights reserved.
+//
+
+import UIKit
+
+class FiltersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
